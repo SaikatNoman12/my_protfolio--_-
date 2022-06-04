2b94fc234f692e15973641d4ab786d7beb721651
SaikatNoman12 <noommaa240@gmail.com>
2022-06-04 20:43:05 +0600

    add about me section. and responsive this section.

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -7,9 +7,13 @@ const routes: Routes = [
     path: '',
     component: PagesComponent,
     children: [
+      // {
+      //   path: '',
+      //   loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+      // },
       {
         path: '',
-        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+        loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
       }
     ]
   }
